436f9a2807dff7fad3448f1a51b6c93a9c19bf25
gabrielnmontini <121122228+gabrielnmontini@users.noreply.github.com>
2023-01-13 14:18:00 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> friends = new List<string> { /* Participants names goes here */};
+List<string> friendsLeft= new(friends);
+List<string> friendsRaffled = new();
+for (int i = 0; i < 12; i++)
+{
+    var raffled = friendsLeft[new Random().Next(0, friendsLeft.Count)];
+    while(friends[i] == raffled) raffled = friendsLeft[new Random().Next(0, friendsLeft.Count)];
+    friendsLeft.Remove(raffled);
+    friendsRaffled.Add(raffled);
+}
+for (int i = 0; i < friends.Count; i++) Console.WriteLine($"{friends[i]} => {friendsRaffled[i]}");
