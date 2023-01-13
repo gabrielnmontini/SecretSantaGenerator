f5ace71f4faafe4b8b29f5a76c3a79f13ee19d38
gabrielnmontini <121122228+gabrielnmontini@users.noreply.github.com>
2023-01-13 14:19:43 -0300

    Making it simpler using .NET Linq features.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,3 @@
-﻿List<string> friends = new List<string> { /* Participants names goes here */};
-List<string> friendsLeft= new(friends);
-List<string> friendsRaffled = new();
-for (int i = 0; i < 12; i++)
-{
-    var raffled = friendsLeft[new Random().Next(0, friendsLeft.Count)];
-    while(friends[i] == raffled) raffled = friendsLeft[new Random().Next(0, friendsLeft.Count)];
-    friendsLeft.Remove(raffled);
-    friendsRaffled.Add(raffled);
-}
+var friends = new List<string> { /* Participants names goes here */ };
+var friendsRaffled = new List<string>(friends).OrderBy(fren => new Random().Next(0, friends.Count - 1)).ToList();
 for (int i = 0; i < friends.Count; i++) Console.WriteLine($"{friends[i]} => {friendsRaffled[i]}");
